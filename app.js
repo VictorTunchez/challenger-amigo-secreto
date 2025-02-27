73ba700455a9dc4a8580f0f40851774fbe88a411
Victor Tunchez <victortunchez2003@gmail.com>
2025-02-27 10:55:58 -0600

    Mejora de la funcion ingresar y mostrar nombres

--- app.js
+++ app.js
@@ -4,19 +4,27 @@ let listaNombresASortear = [];
 //capturamos los nombres del input y los guardamos en un arreglo
 function agregarAmigo(){
     let nombreIngresado = document.getElementById('amigo').value;
+    if(nombreIngresado.trim()===""){
+        alert("Por favor, inserta un nombre valido");
+    }
+
     listaNombresASortear.push(nombreIngresado);
     mostrarNombres();
+    nombreIngresado = "";
+    
 }
 
 //mostramos los nombres en la etiqueta lista 
-//falta resolver el problema de mostrar todos los nombres cada que se actualiza
-//solo se actualiza mostrando el ultimo, no funciono tampoco con el for
 function mostrarNombres(){
-    let ultimoIngresado = listaNombresASortear.length-1;
-    let nombres = document.getElementById('listaAmigos');
-    nombres.innerHTML = listaNombresASortear[ultimoIngresado]; 
-    console.log(listaNombresASortear[ultimoIngresado]);
-  
+    let listaNombres = document.getElementById('listaAmigos');
+    listaNombres.innerHTML = ""
+     
+    for (let index = 0; index < listaNombresASortear.length; index++) {
+        let elementoLista = document.createElement('li');
+        elementoLista.textContent = listaNombresASortear[index];
+        listaNombres.appendChild(elementoLista);
+    }
+    
 }
 
 
